c64eeac9c88bdd05484f4b5dc79209b448d19608
Prev-I <claudio.previtali.88@gmail.com>
2018-09-25 14:17:38 +0200

    Added AesCryptUtil to encrypt/decrypt aes file Added required dependecies

---
+++ Solution.Tools/Utilities/AesCryptUtil.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using log4net;
+
+
+namespace Solution.Tools.Utilities
+{
+    public static class AesCryptUtil
+    {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+
+        public static byte[] DecryptFile(string password, byte[] inputData)
+        {
+            MemoryStream result = null;
+
+            try
+            {
+                using (MemoryStream streamInput = new MemoryStream(inputData))
+                {
+                    result = new MemoryStream();
+                    SharpAESCrypt.SharpAESCrypt.Decrypt(password, streamInput, result);
+                }
+                return result.ToArray();
+            }
+            catch (Exception e)
+            {
+                log.Error(e.Message, e);
+                throw new Exception("Escalated exception", e);
+            }
+            finally
+            {
+                result?.Dispose();
+            }
+        }
+
+        public static void DecryptFile(string password, string inputFileName, string outputFileName)
+        {
+            try
+            {
+                SharpAESCrypt.SharpAESCrypt.Decrypt(password, inputFileName, outputFileName);
+            }
+            catch (Exception e)
+            {
+                log.Error(e.Message, e);
+                throw new Exception("Escalated exception", e);
+            }
+        }
+
+        public static Stream DecryptFile(string password, Stream inputFileStream)
+        {
+            MemoryStream resultStream = new MemoryStream();
+
+            try
+            {
+                SharpAESCrypt.SharpAESCrypt.Decrypt(password, inputFileStream, resultStream);
+                return resultStream;
+            }
+            catch (Exception e)
+            {
+                log.Error(e.Message, e);
+                throw new Exception("Escalated exception", e);
+            }
+        }
+
+
+        public static byte[] EncryptFile(string password, byte[] inputData)
+        {
+            MemoryStream result = null;
+
+            try
+            {
+                using (MemoryStream streamInput = new MemoryStream(inputData))
+                {
+                    result = new MemoryStream();
+                    SharpAESCrypt.SharpAESCrypt.Encrypt(password, streamInput, result);
+                }
+                return result.ToArray();
+            }
+            catch (Exception e)
+            {
+                log.Error(e.Message, e);
+                throw new Exception("Escalated exception", e);
+            }
+            finally
+            {
+                result?.Dispose();
+            }
+        }
+
+        public static void EncryptFile(string password, string inputFileName, string outputFileName)
+        {
+            try
+            {
+                SharpAESCrypt.SharpAESCrypt.Encrypt(password, inputFileName, outputFileName);
+            }
+            catch (Exception e)
+            {
+                log.Error(e.Message, e);
+                throw new Exception("Escalated exception", e);
+            }
+
+        }
+
+        public static Stream EncryptFile(string password, Stream inputFileStream)
+        {
+            MemoryStream resultStream = new MemoryStream();
+
+            try
+            {
+                SharpAESCrypt.SharpAESCrypt.Encrypt(password, inputFileStream, resultStream);
+                return resultStream;
+            }
+            catch (Exception e)
+            {
+                log.Error(e.Message, e);
+                throw new Exception("Escalated exception", e);
+            }
+        }
+
+    }
+}
