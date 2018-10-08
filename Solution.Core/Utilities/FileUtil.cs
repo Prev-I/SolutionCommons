b4bf77f0415edd7525e06410757af915e7a9ef94
Prev-I <claudio.previtali.88@gmail.com>
2018-10-08 16:55:49 +0200

    Added methods to check if file is locked or is growing in size

--- Solution.Core/Utilities/FileUtil.cs
+++ Solution.Core/Utilities/FileUtil.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Threading;
 
 using log4net;
 
@@ -182,6 +183,71 @@ namespace Solution.Core.Utilities
                 log.Error(e.Message, e);
                 throw new Exception("Escalated exception", e);
             }
+        }
+
+        /// <summary>
+        /// Lock a file if its length is not changing during specified interval and the file is not already in use.
+        /// </summary>
+        /// <param name="file">FileInfo that point to file to be locked</param>
+        /// <param name="lockedFileStream">FileStream handle to the locked file</param>
+        /// <param name="mSecIntervalChecBeforeLocking">Window interval to check for file length changes</param>     
+        /// <param name="access">Specify how to access the locked file</param>
+        /// <returns>true if success or false if the file length is changing or the file is already locked.</returns>
+        /// <exception cref="FileNotFoundException">Thrown when file doesn't exist</exception>
+        public static bool Lock(FileInfo file, out FileStream lockedFileStream, int mSecIntervalChecBeforeLocking = 1000, FileAccess access = FileAccess.Read)
+        {
+            lockedFileStream = null;
+
+            if (!file.Exists)
+                throw new FileNotFoundException();
+
+            if (IsFileSizeChanging(file, mSecIntervalChecBeforeLocking))
+            {
+                return false;
+            }
+            else
+            {
+                try
+                {
+                    lockedFileStream = file.Open(FileMode.Open, access, FileShare.None);
+                }
+                catch (Exception)
+                {
+                    lockedFileStream?.Close();
+                    lockedFileStream?.Dispose();
+                    lockedFileStream = null;
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Unlock the fileStream
+        /// </summary>
+        /// <param name="fileStream">FileStream handle to the locked file</param>
+        public static void UnLock(FileStream fileStream)
+        {
+            fileStream?.Close();
+            fileStream?.Dispose();
+        }
+
+        /// <summary>
+        /// Check if the file size is changing during specified interval 
+        /// </summary>
+        /// <param name="file">FileInfo that point to file to be checked</param>
+        /// <param name="mSecInterval">how many milliseconds wait before picking the file length again</param>
+        /// <exception cref="FileNotFoundException">Thrown when file doesn't exist</exception>
+        /// <returns>true if the file size is chaning, false otherwise</returns>
+        public static bool IsFileSizeChanging(FileInfo file, int mSecInterval = 1000)
+        {
+            if (!file.Exists)
+                throw new FileNotFoundException();
+
+            long lenght = file.Length;
+            Thread.Sleep(mSecInterval);
+            file.Refresh();
+            return !((file.Length - lenght) == 0);
         }
 
     }
